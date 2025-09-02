b1a0c0fb332e43d4b751a8d353988bdd816fda03
wukinnin <nimugonzaga@gmail.com>
2025-09-02 20:11:14 +0800

    Reverted all changes back to version fc2c9c317dfe7122860fa8a765f2ee49033f43cf

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import SetPassword from "./pages/SetPassword";
 import { SessionProvider } from "./contexts/SessionContext";
 import ProtectedRoute from "./components/ProtectedRoute";
-import AppLayout from "./components/layout/AppLayout";
-import Placeholder from "./pages/Placeholder";
 
 const queryClient = new QueryClient();
 
@@ -23,15 +21,10 @@ const App = () => (
         <SessionProvider>
           <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="/set-password" element={<SetPassword />} />
             
-            <Route element={<ProtectedRoute><AppLayout /></ProtectedRoute>}>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/inventory" element={<Placeholder />} />
-              <Route path="/suppliers" element={<Placeholder />} />
-              <Route path="/transactions" element={<Placeholder />} />
-              <Route path="/users" element={<Placeholder />} />
-              <Route path="/settings" element={<Placeholder />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/" element={<Index />} />
+              <Route path="/set-password" element={<SetPassword />} />
             </Route>
 
             <Route path="*" element={<NotFound />} />
